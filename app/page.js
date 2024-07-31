77ed043faf1f19c0944cff9d2932b20fb0e6ed4e
David Fischer <fiscd@seas.upenn.edu>
2024-07-30 21:35:35 -0400

    teams

--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
 
 
+import { useRouter } from 'next/router';
+
 export default function Home() {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    router.push('/joinTeam');
+  };
+
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <h1>Home Page</h1>
+      <button onClick={handleNavigation}>Join Team</button>
+    </div>
   );
 }
